e03e3e7d3f9feb699d0f4f2126395099e1a47605
Mike Grouchy <mgrouchy@gmail.com>
2013-10-22 15:25:42 -0400

    brew install some things

--- setup.sh
+++ setup.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
-#this totally doesn't work anymore
+#install homebrew
+#ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
-echo "Symlinking new dotfiles ..."
-  ln -s .bashrc ~/.bashrc
-  ln -s .bash_profile ~/.bash_profile
-  ln -s .gitconfig ~/.gitconfig
-  ln -s .zshrc ~/.zshrc
-  ln -s .zshenv ~/.zshenv
-  ln -s .zshfunc ~/.zshfunc
-  ln -s .zshalias ~/.zshalias
+#install homebrew things
+brew install wget
+brew install macvim
+brew install libevent
+brew install htop
+brew install rabbitmq
+brew install tmux
+brew install sqlite
+brew install memcached
+brew install libmemcached
+brew install gettext
+brew install ctags
+brew install ack
+brew install jpg
+brew install redis
+brew install readline
+
+echo "Installing Iterm2"
+wget http://iterm2.com/downloads/stable/iTerm2_v1_0_0.zip -P ~/Downloads/
+
+#mv ~/Downloads/iterm2/iterm2.app /Applications/
+
+#echo "Symlinking new dotfiles ..."
+  #ln -s .bashrc ~/.bashrc
+  #ln -s .bash_profile ~/.bash_profile
+  #ln -s .gitconfig ~/.gitconfig
+  #ln -s .zshrc ~/.zshrc
+  #ln -s .zshenv ~/.zshenv
+  #ln -s .zshfunc ~/.zshfunc
+  #ln -s .zshalias ~/.zshalias
 
